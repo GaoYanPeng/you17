b77d1a8260eb4224858157282ff4be1d2eb229df
高鹏 <1598039446@qq.com>
2016-08-29 10:40:39 +0800

    功能二

--- GitHubDemo/app/src/main/java/com/lanou3g/githubdemo/MainActivity.java
+++ GitHubDemo/app/src/main/java/com/lanou3g/githubdemo/MainActivity.java
@@ -2,13 +2,20 @@ package com.lanou3g.githubdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnClickListener{
     //功能一
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onClick(View view) {
+        
     }
 }
